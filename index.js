b0f02665c9d2cc0350c4ffd416a732f3b616f003
aqueleiori <aqueleyori@gmail.com>
2022-03-02 03:32:34 -0300

    update ...

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const app = express()
 app.use(cors())
 app.get('/', (req, res) => {
   res.json({data: "bolo de morango"})
+  res.setHeader('Content-Type', 'application/json');
 })
+
 
 app.listen(5000, () => {
   console.log("listen on 5000")
